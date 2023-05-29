87389b3a3844ad89094046c5e5e4d308c24d7a2a
Eduardo6446 <eduorozco8@gmail.com>
2023-05-28 18:40:16 -0600

    practica2

---
+++ funciones/ejercicio.dart
@@ -0,0 +1,28 @@
+void main() {
+  String content =
+      'Hola mundo, hoy es martes, mañana es. nochebuena, pasado Mañana es navidad. hola mundo Nochebuena eS! el mejor día';
+  print(wordsCounter(content, 'es!.'));
+}
+
+String wordsCounter(String text, String query) {
+  Map<String, int> counterMap = {};
+  List<String> wordsList = text.split(' ');
+
+  for (String word in wordsList) {
+    word = normalize(word);
+
+    if (counterMap.containsKey(word)) {
+      counterMap[word] = counterMap[word]! + 1;
+    } else {
+      counterMap[word] = 1;
+    }
+  }
+
+  query = normalize(query);
+
+  return 'La palabra $query se repite: ${counterMap[query] ?? 0} veces';
+}
+
+String normalize(String word) {
+  return word.toLowerCase().replaceAll(RegExp('[,!.]'), '');
+}
